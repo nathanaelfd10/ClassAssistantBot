142499c63af9a3c27ba1ac73c252e50a55fd7a8c
Fernando N <64892153+noxfl@users.noreply.github.com>
2021-09-07 20:53:50 +0700

    Revision 16
    -Added manual info check
    -Added custom reminder functionality

--- ClassAssistant.py
+++ ClassAssistant.py
@@ -1,12 +1,20 @@
 import json
 from datetime import datetime, timezone, timedelta
+import pytz
 
 class ClassAssistantBot(object):
 
     def __init__(self):
-        with open('schedule.json') as f:
-            self.jadwal_json = json.load(f)
+        self.jadwal_json = self.load_json()
         self.day_list_english = ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday')
+        # self.prune_json()
+        #DATE TIME
+        timezone_offset = +7.0  # Asia/Jakarta (UTC+07:00)
+        tzinfo = timezone(timedelta(hours=timezone_offset))
+        now_tz_jkt = datetime.now(tzinfo)
+        self.today = now_tz_jkt.strftime("%A")
+        self.schedule_json = 'schedule.json'
+        
        
     def listToString(self, s): 
         str1 = ""
@@ -14,21 +22,60 @@ class ClassAssistantBot(object):
             str1 += ele  
         return str1 
 
-    def get_matkul_json(self):
+    def get_jadwal_json(self):
+        self.jadwal_json = self.load_json()
         return self.jadwal_json
+    
+    def save_json(self, dict):
+        with open(self.schedule_json, 'w') as json_file:
+            json.dump(dict, json_file)
+
+    def load_json(self):
+        with open('schedule.json') as f:
+            return json.load(f)
+
+    def prune_json(self):
+        tz = pytz.timezone('Asia/Jakarta')
+        with open('schedule2.json', "a") as json_file:
+            data = json.load(json_file)
+            
+            for x in data['jadwal_tugas']:
+                now = datetime.now()
+                now_jkt = now.replace(tzinfo=tz)
+                date_parse = datetime.strptime(x['date_end'], "%d %B, %Y - %H:%M")
+                date_parse_tz = date_parse.replace(tzinfo=tz)
+                if date_parse_tz < now_jkt:
+                    del x
+        
+            json.dump(data, 'schedule2.json')
 
     def validate_day(self, day):
-        
         if day in self.day_list_english:
             return True
         else:
             return False
+
+    def get_tugas_schedule(self, day):
+        self.jadwal_json = self.load_json()
+        tugas_list = []
+        show_all_template = "{no}. {matkul_name} — {day_end} - {tugas_name}"
+        list_content_template = "{no}. {matkul_name} — {tugas_name} - {day_end} - {date_end} | {desc}"
+        list_counter = 0
+        for tugas in self.jadwal_json['jadwal_tugas']:
+            if str(tugas['day_end']).upper() == str(day).upper():
+                list_counter += 1
+                tugas_list.append(list_content_template.format(
+                    no = list_counter,
+                    matkul_name = tugas['matkul_name'],
+                    tugas_name = tugas['tugas_name'],
+                    date_end = tugas['date_end'],
+                    day_end = tugas['day_end'],
+                    desc = tugas['desc']
+                ))
+        return tugas_list
     
     def get_matkul_schedule(self, day):
-            timezone_offset = +7.0  # Asia/Jakarta (UTC+07:00)
-            tzinfo = timezone(timedelta(hours=timezone_offset))
-            now_tz_jkt = datetime.now(tzinfo)
-            self.today = now_tz_jkt.strftime("%A")
+            self.jadwal_json = self.load_json()
             matkul_list = []
             show_all_template = "{no}. {day} — {name} - {hour_begin}-{hour_end}"
             list_content_template = "{no}. {kode_matkul} — {name} - {hour_begin}-{hour_end}"
@@ -83,6 +130,30 @@ class ClassAssistantBot(object):
                 print("Output from get_matkul_schedule():\n", matkul_list)
                 return matkul_list
 
+    def tugas_data_to_message(self, tugas_list, day):
+        tugas_greetings_template = "======== {}'s Tasks ========\n"
+        if str(day).upper() == 'TODAY':
+            tugas_greetings = tugas_greetings_template.format(self.today)
+        elif self.validate_day(day):
+            tugas_greetings = tugas_greetings_template.format(day)
+        elif str(day).upper() == 'LIST':
+            tugas_greetings = "======== Showing All Tasks ========\n"
+        else:
+            tugas_greetings = "```{} is not a valid day of week name.\nPlease enter a valid day of week in English/Indonesian\n(e.g. Monday or Senin)```".format(day)
+            return tugas_greetings
+
+        tugas_message = []
+        tugas_message.append(tugas_greetings)
+        if len(tugas_list) == 0:
+            tugas_message.append("No tasks for {} of this week, rest easy!".format(day))
+        else:
+            for tugas in tugas_list:
+                tugas_message.append(str(tugas+"\n"))
+        print("From get_tugas_list_message:\n", self.listToString(tugas_message))
+        tugas_message_string = "```{}```".format(self.listToString(tugas_message))
+        return tugas_message_string
+
+
     def matkul_data_to_message(self, matkul_list, day):
         matkul_greetings_template = "======== {}'s Classes ========\n"
         if str(day).upper() == 'TODAY':
@@ -105,13 +176,6 @@ class ClassAssistantBot(object):
         print("From get_matkul_list_message:\n", self.listToString(matkul_message))
         matkul_message_string = "```{}```".format(self.listToString(matkul_message))
         return matkul_message_string
-
-    trigger_word = ["!ifbot", "kalbiser!"]
-
-    response_words = [
-        "Cheer up!",
-        "Yo!"
-    ]
 
 
     
--- main.py
+++ main.py
@@ -4,10 +4,9 @@ from discord.ext import commands
 import discord
 import os
 from dotenv import load_dotenv
-import random
 import requests
 import json
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 import pytz
 from ClassAssistant import ClassAssistantBot
 
@@ -20,12 +19,14 @@ CHANNEL_ID = 879663995852849193 #DEV Server
 
 #INITIALIZE THE ACTUAL PROGRAM
 IFBot = ClassAssistantBot()
+tugas_schedule_list = IFBot.get_jadwal_json()
 
 #INITIALIZE DATE TIME
 now = datetime.now()
 tz = pytz.timezone('Asia/Jakarta')
 tz_jkt = now.replace(tzinfo=tz)
 day_list = {'Minggu':'Sunday', 'Senin':'Monday', 'Selasa':'Tuesday', 'Rabu':'Wednesday', 'Kamis':'Thursday', 'Jumat':'Friday', 'Sabtu':'Saturday'}
+deadline_format = "%d %B, %Y - %H:%M"
 
 print(tz_jkt)
 
@@ -43,7 +44,128 @@ async def today(ctx):
     c = bot.get_channel(CHANNEL_ID)
     await c.send(matkul_info_message)
 
-@bot.command(name='class')
+@bot.command(name='task_new')
+async def new_task(ctx, matkul_name, tugas_name, desc, deadline):
+    tugas_schedule_list = IFBot.get_jadwal_json()
+    tugas_schedule_list['jadwal_tugas'].append(
+        {      
+            "matkul_name": str(matkul_name).capitalize(),
+            "tugas_name": str(tugas_name).capitalize(),
+            "desc": str(desc).capitalize(),
+            "date_end": str(deadline),
+            "day_end": datetime.strptime(deadline, deadline_format).strftime('%A')
+        }
+    )
+    IFBot.save_json(tugas_schedule_list)
+    await add_tugas_reminder(matkul_name, tugas_name, desc, deadline)
+
+    c = bot.get_channel(CHANNEL_ID)
+    await c.send("Task {} successfully created.".format(tugas_name))
+    
+async def add_tugas_reminder(matkul_name, tugas_name, desc, deadline):
+    deadline_time = datetime.strptime(deadline, deadline_format)
+    time_morning = deadline_time.replace(hour=10, minute=00)
+    scheduler.add_job(send_tugas_embed, 'date', run_date=deadline_time, timezone=tz, 
+    args=[matkul_name, tugas_name, desc, deadline])
+
+async def send_tugas_embed(matkul_name, tugas_name, desc, deadline):
+    embed=discord.Embed(title="Task Reminder", description="The following task will be due in a few days:".format(tugas_name=tugas_name), color=0x0091ff)
+    embed.set_author(name="ClassAssistantBot", url="https://github.com/noxfl/ClassAssistantBot/", icon_url="https://avatars.githubusercontent.com/u/64892153?v=4")
+    embed.add_field(name="{matkul_name}".format(matkul_name=matkul_name), value="{tugas_name}: {desc}\n{deadline} ({day_end})".format(
+        tugas_name = tugas_name,
+        desc = desc,
+        deadline = deadline,
+        day_end = await get_day(deadline, deadline_format)
+    ), inline=False)
+    c = bot.get_channel(CHANNEL_ID)
+    await c.send(embed=embed)
+
+async def get_day(date, deadline_format):
+    return datetime.strptime(date, deadline_format).strftime('%A')
+
+async def remove_all_scheduler():
+    for job in scheduler.get_jobs():
+        print(job)
+        job.remove()
+
+@bot.command(name='task_reload')
+async def reload_scheduler(ctx):
+    print('\n======== REMOVED ALL SCHEDULER ========')
+    await remove_all_scheduler()
+    
+    print('\n======== LOADED MATKUL REMINDER ========')
+    await populate_matkul_reminder()
+    
+    print('\n======== LOADED TUGAS REMINDER ========')
+    await populate_tugas_reminder()
+    
+    c = bot.get_channel(CHANNEL_ID)
+    await c.send("Tasks successfully reloaded.")
+    
+@bot.command(name='absen')
+async def absensi2(ctx, i=0):
+        count = 0
+        today_jadwal = {}
+        jadwal = tugas_schedule_list['jadwal_mobile']
+
+        for x in jadwal:
+            if x['day'] == IFBot.today:
+                count += 1
+                today_jadwal[count] = x
+        await send_absen_embed(today_jadwal, i)
+
+async def send_absen_embed(jadwal, i):
+    c = bot.get_channel(CHANNEL_ID)
+    try:
+        embed=discord.Embed(title="{kode_matkul} {name} | {hour_begin} - {hour_end}.".format(
+          kode_matkul = jadwal[i]['kode_matkul'],
+          name = jadwal[i]['name'],
+          hour_begin = jadwal[i]['hour_begin'],
+          hour_end = jadwal[i]['hour_end'],
+        ), description="desc", color=0x3dff54)
+        embed.set_author(name="ClassAssistantBot", url="https://github.com/noxfl", icon_url="https://avatars.githubusercontent.com/u/64892153?v=4")
+        embed.add_field(name="Be sure to login first before clicking any of the link below.", value="[Login](http://leaps.kalbis.ac.id/login)", inline=False)
+        embed.add_field(name="Attendance", value="[Tap In/Tap Out]({tap_in_link})".format(tap_in_link = jadwal[i]['tap_in_link']), inline=True)
+        embed.add_field(name="TLM", value="[Here]({learning_resource})".format(learning_resource = jadwal[i]['learning_resource']), inline=True)
+        embed.add_field(name="Assignments", value="[Here]({assignment})".format(assignment = jadwal[i]['assignment']), inline=True)
+        embed.set_footer(text="{lecturer}".format(lecturer=jadwal[i]['lecturer']))
+        
+        await c.send(embed=embed)
+        print('Embed sent: {name}'.format(name=jadwal[i]['name']))
+    except: 
+        matkul_info_message = IFBot.matkul_data_to_message(IFBot.get_matkul_schedule('TODAY'), 'TODAY')
+        await c.send(matkul_info_message)
+
+@bot.command(name='task', aliases=['tugas', 'deadline'])
+async def task_deadline(ctx, arg=None, matkul_name="", tugas_name="", desc="", deadline=""):
+    c = bot.get_channel(CHANNEL_ID)
+    if str(arg).upper() == 'NEW':
+        try:
+            deadline_dt = datetime.strptime(deadline, deadline_format)
+            deadline_jkt = deadline_dt.replace(tzinfo=tz)
+            # deadline = "21 June, 2021 - 10:00"
+            # deadline_dt = datetime.strptime(deadline, deadline_format)
+            # deadline_jkt = deadline_dt.replace(tzinfo=tz)
+            print(deadline_dt)
+            await c.send(deadline_jkt)
+        except:
+            await c.send("```Please enter a valid format\n!task <day|new> <matkul_name> <tugas_name> <desc> <deadline>```")
+    else:
+        arg_c1 = str(arg).capitalize()
+
+        if arg_c1 in day_list:
+            arg_c2 = str(day_list.get(arg_c1)).capitalize()
+            print('arg in list', arg_c2)
+        else: 
+            arg_c2 = str(arg).capitalize()
+
+        print('ARG_C: ', arg_c1)
+        print('INPUT 2: ', arg)
+
+        tugas_info_message = IFBot.tugas_data_to_message(IFBot.get_tugas_schedule(arg_c2), arg_c2)
+        await c.send(tugas_info_message)
+
+@bot.command(name='class', aliases=['kelas', 'cek', 'jadwal'])
 async def day(ctx, arg):
 
     #Checks if user asks for HELP, else run program
@@ -101,8 +223,29 @@ def modify_thirty_minutes(hour, mode):
     time_converted = datetime.strftime(time_parsed, "%H:%M")
     return time_converted
 
+
+async def populate_tugas_reminder():
+    tugas_schedule_list = IFBot.get_jadwal_json()
+    tugas_set_count = 0
+    for tugas in tugas_schedule_list['jadwal_tugas']:
+        tugas_set_count += 1
+        print('Adding {name} to schedule..'.format(name=tugas['tugas_name']))
+        await add_tugas_reminder(tugas['matkul_name'], tugas['tugas_name'], tugas['desc'], tugas['date_end'])
+        # deadline_time = datetime.strptime(tugas['date_end'], deadline_format)
+        # time_morning = deadline_time.replace(hour=10, minute=00)
+        # time_h_min_tiga_morning = deadline_time.replace() #pokoknya mundur 3 hari lah dia
+        # scheduler.add_job(send_message, 'date', run_date=time_morning, timezone=tz, args=['text'])
+        
+
+    print('Tugas set count:', tugas_set_count)
+
+async def show_scheduler():
+    for job in scheduler.get_jobs():
+        print(job.name, job.trigger, job.func)
+
+
 async def populate_matkul_reminder():
-    matkul_schedule_list = IFBot.get_matkul_json()
+    matkul_schedule_list = IFBot.get_jadwal_json()
     matkul_set_count = 0
     tap_in_message = "Tap in here"
     tap_out_message = "Tap out here"
@@ -122,14 +265,12 @@ async def populate_matkul_reminder():
             args=[matkul['kode_matkul'], matkul['name'], matkul['hour_begin'], matkul['hour_end'], 
                    tap_out_message, matkul_end_reminder_message, matkul['tap_out_link'], matkul['lecturer'], matkul['learning_resource'], matkul['assignment']])
     print('Matkul set count:', matkul_set_count)
-    for job in scheduler.get_jobs():
-        print(job.name, job.trigger, job.func)
 
-async def send_matkul_reminder(matkul):
+async def send_message(message):
     try:
         c = bot.get_channel(CHANNEL_ID)
-        await c.send(matkul)
-        print('Message sent: {}'.format(matkul))
+        await c.send(message)
+        print('Message sent: {}'.format(message))
     except:
         print('Message delivery failed')
 
@@ -153,7 +294,8 @@ async def on_ready():
     print('Successfully logged in as {0.user}'.format(bot))
     print("Ready..")
     await populate_matkul_reminder()
-    # await send_matkul_reminder('Bot has come online')
+    await populate_tugas_reminder()
+    # await send_message('Bot has come online')
     scheduler.start()
     
 
